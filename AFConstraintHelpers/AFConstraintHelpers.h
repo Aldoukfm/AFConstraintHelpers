c75eefdc68c82f1414a6e4aee8a7bd83d36bb1eb
Aldo Fuentes <35090917+Aldoukfm@users.noreply.github.com>
2019-05-18 13:50:36 -0500

    Initial Commit

---
+++ AFConstraintHelpers/AFConstraintHelpers.h
@@ -0,0 +1,19 @@
+//
+//  AFConstraintHelpers.h
+//  AFConstraintHelpers
+//
+//  Created by Aldo Fuentes on 5/18/19.
+//  Copyright © 2019 softtek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFConstraintHelpers.
+FOUNDATION_EXPORT double AFConstraintHelpersVersionNumber;
+
+//! Project version string for AFConstraintHelpers.
+FOUNDATION_EXPORT const unsigned char AFConstraintHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFConstraintHelpers/PublicHeader.h>
+
+
